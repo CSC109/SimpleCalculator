8a96c5f24ca67230ce5d3c274fcdbb40d7f52752
a-r-t <alexthimineur@gmail.com>
2021-02-02 16:48:42 -0500

    initial commit

---
+++ src/SimpleCalculator.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class SimpleCalculator {
+	public static void main(String[] args) {
+		Scanner keyboardInput = new Scanner(System.in);
+		System.out.println("This is a calculator!");
+		System.out.println("Enter in operand 1:");
+		int operand1 = keyboardInput.nextInt();
+		System.out.println("Enter in operand 2:");
+		int operand2 = keyboardInput.nextInt();
+		System.out.println("Enter in desired operator (choices are +, -, *, and /");
+		keyboardInput.nextLine();
+		char operator = keyboardInput.nextLine().charAt(0);
+		
+		int result = 0;
+		if (operator == '+') {
+			result = operand1 + operand2;
+		}
+		else if (operator == '-') {
+			result = operand1 - operand2;
+		}
+		else if (operator == '*') {
+			result = operand1 - operand2;
+		}
+		else if (operator == '/') {
+			result = operand1 - operand2;
+		}
+		
+		System.out.println(operand1 + " " + operator + " " + operand2 + " = " + result);
+	}
+}
